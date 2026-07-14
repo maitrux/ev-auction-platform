e5313fc6963881af01e837920fd11b1be73ce251
Mai Trux <maitrux@hotmail.com>
2026-07-14 15:55:20 +0200

    First endpoint GET users/:id

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  getUserById(id: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+}
